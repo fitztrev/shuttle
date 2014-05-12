56c3d88e79d6f92bf3f9408bd8dbdace0393fb6e
Matt Turner <matt@thshdwknws.net>
2014-05-11 18:11:24 -0700

    Added About Window
    When the user clicks the About button on the menu a new window is
    opened. This window gets is data values from the Info.plist file and
    shows the program name, version, copyright and a button to go to the
    homepage.

---
+++ Shuttle/AboutWindowController.m
@@ -0,0 +1,77 @@
+//
+//  AboutWindowController.m
+//  Shuttle
+//
+//  Created by Matthew Turner on 5/9/14.
+//  Copyright (c) 2014 fitztrev. All rights reserved.
+//
+
+#import "AboutWindowController.h"
+
+@interface AboutWindowController ()
+
+@end
+
+@implementation AboutWindowController
+@synthesize aboutWindow;
+@synthesize appName;
+@synthesize appVersion;
+@synthesize appCopyright;
+
+NSDictionary *plistDict;
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    aboutWindow = [super initWithWindow:window];
+    if (self) {
+    // Initialization code here.
+    }
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    //Load the plist so we can get current info for the about box.
+    plistDict = [[NSBundle mainBundle] infoDictionary];
+    
+    //Get the application name.
+    id applicationName = [plistDict objectForKey:@"CFBundleName"];
+    //Get the build version.
+    id applicationVersion = [plistDict objectForKey:@"CFBundleVersion"];
+    //Get the copyright.
+    id applicationCopyright = [plistDict objectForKey:@"NSHumanReadableCopyright"];
+    
+    //Build the string for the windows title.
+    NSString *aboutTitle = [NSString stringWithFormat:@"%@%@", @"About ", applicationName];
+    [aboutWindow.window setTitle:aboutTitle];
+    
+    //Build the string for the application name. appName - tagline
+    NSString *progName = [NSString stringWithFormat:@"%@%@", applicationName, @" - A simple SSH shortcut menu."];
+    [appName setStringValue:progName];
+    
+    //Build the string for the version. Version: $build
+    NSString *progVersion = [NSString stringWithFormat:@"%@%@", @"Version: ", applicationVersion];
+    [appVersion setStringValue:progVersion];
+    
+    //Make the copyright font smaller.
+    [appCopyright setFont:[NSFont systemFontOfSize:10]];
+    [appCopyright setStringValue:applicationCopyright];
+
+}
+
+- (IBAction)btnHomepage:(id)sender {
+    
+    //Get the homepage from the plist
+    id applicationHomepage = [plistDict objectForKey:@"Product Homepage"];
+    //Build the homepage's URL.
+    NSURL *homeURL = [NSURL URLWithString:applicationHomepage];
+    
+    //Go to the website.
+    [[NSWorkspace sharedWorkspace] openURL:homeURL];
+    
+    //Close the about box.
+    [aboutWindow close];
+}
+@end
